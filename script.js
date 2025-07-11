5c248dfebd414dc4700588bfaf54ee0728472522
Gabriel Ryan <gabrielryanf.t@gmail.com>
2025-07-11 08:14:46 -0300

    add info card
    add script to open it in script.js
    add style for it in style.js
    and add the info card in index.html

--- script.js
+++ script.js
@@ -4093,3 +4093,16 @@ function bloom() {
         }, 45)
     }, 1500);
 }
+
+const modal = document.querySelector("#modal")
+const abrir_modal = document.querySelector("#abrir_modal")
+const fechar_modal = document.querySelector("#fechar_modal")
+
+abrir_modal.addEventListener("click", () =>{
+    modal.showModal()
+    console.log("Modal aberto!")
+})
+fechar_modal.addEventListener("click", () => {
+    modal.close()
+    console.log("Modal fechado!")
+})
